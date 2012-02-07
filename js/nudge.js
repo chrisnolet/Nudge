ba1345dedaddadeb6147b807aa5fd0950aed5efd
Chris Nolet <chris@skiggle.com.au>
2012-02-08 10:34:55 +1100

    Add Margin and Padding placement code, add output box

--- js/nudge.js
+++ js/nudge.js
@@ -24,6 +24,7 @@
 
 // Global variables
 var _target = null;
+var _mode = "position";
 
 
 // jQuery ready
@@ -32,52 +33,150 @@ $(document).ready(function() {
 	// Add Bootstrap
 	//var bootstrap = '<link rel="stylesheet" href="https://raw.github.com/chrisnolet/Nudge/master/css/bootstrap.min.css"/>';
 	var bootstrap = '<link rel="stylesheet" href="http://twitter.github.com/bootstrap/1.4.0/bootstrap.min.css"/>';
-	$('head').append(bootstrap);
+	$("head").append(bootstrap);
 	
 	// Add topbar
 	var topbar = '\
 		<div class="topbar">\
 			<div class="fill">\
 				<div class="container">\
-					<a class="brand" href="#">Nudge</a>\
+					<div class="brand" style="cursor: default;">Nudge</div>\
 					<ul class="nav">\
-						<li class="active"><a href="#">Position</a></li>\
-						<li><a href="#">Margin</a></li>\
-						<li><a href="#">Padding</a></li>\
+						<li id="nudge-position" class="active"><a id="nudge-position-link" href="#">Position</a></li>\
+						<li id="nudge-margin"><a id="nudge-margin-link" href="#">Margin</a></li>\
+						<li id="nudge-padding"><a id="nudge-padding-link" href="#">Padding</a></li>\
+					</ul>\
+					<ul class="nav secondary-nav">\
+						<li class="dropdown"><a href="#" id="nudge-position-style" class="dropdown-toggle">Position: Static</a>\
+							<ul class="dropdown-menu">\
+								<li><a id="nudge-static-link" href="#">Static</a></li>\
+								<li><a id="nudge-relative-link" href="#">Relative</a></li>\
+								<li><a id="nudge-absolute-link" href="#">Absolute</a></li>\
+								<li><a id="nudge-fixed-link" href="#">Fixed</a></li>\
+								<li></li>\
+								<li><a id="nudge-inherit-link" href="#">Inherit</a></li>\
+							</ul>\
+						</li>\
 					</ul>\
 				</div>\
 			</div>\
 		</div>\
 	';
-	$('body').append(topbar);
+	$("body").append(topbar).dropdown();
 	
 	// Add selection box
 	var box = '\
-		<div class="nudge-box" style="\
+		<div class="nudge-box" class="hide" style="\
 			position: absolute;\
-			border: 1px solid blue;\
+			margin: 0;\
+			padding: 0;\
+			border: 1px solid #00f;\
 			background-color: #8bb6dd;\
 			opacity: 0.5;\
-			z-index: 1000;\
-		"></div>\
+			z-index: 10000;\
+		">\
+			<p style="\
+				position: absolute;\
+				margin: 0;\
+				padding: 0;\
+				left: 4px;\
+				top: 4px;\
+				font: 15px \'Helvetica Neue\', Helvetica, Arial, sans-serif;\
+				color: #005;\
+			"></p>\
+		</div>\
 	';
-	$('body').append(box);
+	$("body").append(box);
+	
+	// Add output box
+	var output = '\
+		<div id="nudge-output" class="hide" style="\
+			position: fixed;\
+			margin: 0;\
+			padding: 0;\
+			top: 40px;\
+			right: 0;\
+			border: 1px solid #00f;\
+			background-color: #8bb6dd;\
+			opacity: 0.5;\
+			z-index: 100000;\
+		">\
+			<p id="nudge-output-paragraph" style="\
+				margin: 0;\
+				padding: 8px;\
+				font: 13px Monaco, \'Lucida Console\', \'Courier New\', monospace, sans-serif;\
+				color: #005;\
+			"></p>\
+		</div>\
+	';
+	$('body').append(output);
+	
+	// Toolbar actions
+	$("#nudge-position").click(function(e) {
+		e.preventDefault();
+		setMode("position");
+	});
+	$("#nudge-margin").click(function(e) {
+		e.preventDefault();
+		setMode("margin");
+	});
+	$("#nudge-padding").click(function(e) {
+		e.preventDefault();
+		setMode("padding");
+	});
+	$("#nudge-static").click(function(e) {
+		e.preventDefault();
+		mode(1);
+	});
+	$("#nudge-relative").click(function(e) {
+		e.preventDefault();
+		mode(1);
+	});
+	$("#nudge-absolute").click(function(e) {
+		e.preventDefault();
+		mode(1);
+	});
+	$("#nudge-fixed").click(function(e) {
+		e.preventDefault();
+		mode(1);
+	});
+	$("#nudge-inherit").click(function(e) {
+		e.preventDefault();
+		mode(1);
+	});
 	
 	// Attach click handler
 	var elements = 'h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, cite, code, del, dfn, em, img, q, s, samp, small, strike, strong, sub, sup, tt, var, dd, dl, dt, li, ol, ul, fieldset, form, label, legend, button, table, caption, tbody, tfoot, thead, tr, th, td, div, footer, input';
 	$(elements).mousedown(onClick);
 	
 	// Attach keypress handler
-	$('body').keydown(onKeyDown);
+	$("body").keydown(onKeyDown);
 });
 
+// Save mode selection
+function setMode(mode) {
+	
+	// Deselect the previous mode
+	$("#nudge-" + _mode).removeClass("active");
+	
+	// Remember the mode
+	_mode = mode;
+	
+	// Hightlight the selected mode
+	$("#nudge-" + _mode).addClass("active");
+}
+
+// Element clicked
 function onClick(event) {
+	
+	console.log(event.target.id);
+	// Skip and do nothing if element id begins with 'nudge-'
+	if (event.target.id.lastIndexOf('nudge-', 0) === 0) {
+		return;
+	}
 	
 	// Prevent default click action
 	event.preventDefault();
-	
-	// Get jQuery object for the highlight box
-	var box = $(".nudge-box");
 	
 	// Warn and skip if item does not have an element id
 	if (event.target.id === "") {
@@ -86,7 +185,8 @@ function onClick(event) {
 		_target = null;
 		
 		// Hide the highlight box
-		box.hide();
+		$(".nudge-box").hide();
+		$("#nudge-output").hide();
 		
 		return;
 	}
@@ -98,13 +198,47 @@ function onClick(event) {
 	var object = $("#" + _target.id);
 	
 	// Show the highlight box
-	box.offset(object.position());
+	moveHightlightBox(object).show();
+	$("#nudge-output").show();
+}
+
+function moveHightlightBox(object) {
+	
+	// Get jQuery object for the highlight box
+	var box = $(".nudge-box");
+	
+	box.offset(object.offset());
 	box.width(object.innerWidth());
 	box.height(object.innerHeight());
 	
-	box.show();
+	// Set the label text
+	//$(".nudge-box > p").html("<b>#" + _target.id + "</b> (" + object.innerWidth() + ", " + object.innerHeight() + ")");
+	
+	// Set the output text
+	$("#nudge-output > p").html("<b>#" + _target.id + "</b> {<br/>"
+		+ "&nbsp;&nbsp;&nbsp; margin-left: " + object.css("margin-left") + ";<br/>"
+		+ "&nbsp;&nbsp;&nbsp; margin-top: " + object.css("margin-top") + ";<br/>"
+		+ "&nbsp;&nbsp;&nbsp; padding-left: " + object.css("padding-left") + ";<br/>"
+		+ "&nbsp;&nbsp;&nbsp; padding-top: " + object.css("padding-top") + ";<br/>"
+		+ "&nbsp;&nbsp;&nbsp; left: " + object.css("left") + ";<br/>"
+		+ "&nbsp;&nbsp;&nbsp; top: " + object.css("top") + ";<br/>"
+		+ "&nbsp;&nbsp;&nbsp; width: " + object.css("width") + ";<br/>"
+		+ "&nbsp;&nbsp;&nbsp; height: " + object.css("height") + ";<br/>"
+		+ "}<br/><br/>"
+		+ '<a id="nudge-copy" href="#" style="color: #005;">Copy to clipboard</a>'
+	);
+	
+	// 'Copy to Clipboard' clicked
+	$("#nudge-copy").click(function() {
+		
+		// TODO(CN): Write copy code here.
+	});
+	
+	// Return the jQuery object for chaining
+	return box;
 }
 
+// Key pressed
 function onKeyDown(event) {
 	
 	// Skip if target is null
@@ -112,34 +246,79 @@ function onKeyDown(event) {
 		return;
 	}
 	
+	// Adjust action for different modifiers
+	var sizeModifier = (event.altKey || event.ctrlKey || event.metaKey);
+	var speedModifier = event.shiftKey;
+	
+	// Choose attribute names based on the current mode
+	var xAttribute;
+	var yAttribute;
+	
+	switch (_mode) {
+		case "position":
+			xAttribute = "left";
+			yAttribute = "top";
+			break;
+			
+		case "margin":
+			xAttribute = "margin-left";
+			yAttribute = "margin-top";
+			break;
+			
+		case "padding":
+			xAttribute = "padding-left";
+			yAttribute = "padding-top";
+			break;
+	}
+	
 	// Get jQuery object for target
 	var object = $("#" + _target.id);
-	
-	// Adjust action for different modifiers
-	var sizeModifier = (event.altKey || event.metaKey);
-	var speedModifier = event.shiftKey;
 	
 	// Respond to arrow keys and modifiers
 	switch (event.keyCode) {
 		
 		// Left key
 		case 37:
-			object.css((sizeModifier ? "width" : "left"), "-=" + (speedModifier ? 10 : 1));
+		case 83:
+			object.css((sizeModifier ? "width" : xAttribute), "-=" + (speedModifier ? 10 : 1));
 			break;
 			
 		// Up key
 		case 38:
-			object.css((sizeModifier ? "height" : "top"), "-=" + (speedModifier ? 10 : 1));
+		case 69:
+			object.css((sizeModifier ? "height" : yAttribute), "-=" + (speedModifier ? 10 : 1));
 			break;
 			
 		// Right key
 		case 39:
-			object.css((sizeModifier ? "width" : "left"), "+=" + (speedModifier ? 10 : 1));
+		case 70:
+			object.css((sizeModifier ? "width" : xAttribute), "+=" + (speedModifier ? 10 : 1));
 			break;
 			
 		// Down key
 		case 40:
-			object.css((sizeModifier ? "height" : "top"), "+=" + (speedModifier ? 10 : 1));
+		case 68:
+			object.css((sizeModifier ? "height" : yAttribute), "+=" + (speedModifier ? 10 : 1));
+			break;
+		
+		// Resize-left key
+		case 74:
+			object.css("width", "-=" + (speedModifier ? 10 : 1));
+			break;
+			
+		// Resize-up key
+		case 73:
+			object.css("height", "-=" + (speedModifier ? 10 : 1));
+			break;
+			
+		// Resize-right key
+		case 76:
+			object.css("width", "+=" + (speedModifier ? 10 : 1));
+			break;
+			
+		// Resize-down key
+		case 75:
+			object.css("height", "+=" + (speedModifier ? 10 : 1));
 			break;
 		
 		default:
@@ -147,9 +326,62 @@ function onKeyDown(event) {
 	}
 	
 	// Reposition the highlight box
-	var box = $(".nudge-box");
-	
-	box.offset(object.position());
-	box.width(object.innerWidth());
-	box.height(object.innerHeight());
+	moveHightlightBox(object);
 }
+
+
+/* ============================================================
+ * bootstrap-dropdown.js v1.4.0
+ * http://twitter.github.com/bootstrap/javascript.html#dropdown
+ * ============================================================
+ * Copyright 2011 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============================================================ */
+
+
+!function( $ ){
+
+  "use strict"
+
+  /* DROPDOWN PLUGIN DEFINITION
+   * ========================== */
+
+  $.fn.dropdown = function ( selector ) {
+    return this.each(function () {
+      $(this).delegate(selector || d, 'click', function (e) {
+        var li = $(this).parent('li')
+          , isActive = li.hasClass('open')
+
+        clearMenus()
+        !isActive && li.toggleClass('open')
+        return false
+      })
+    })
+  }
+
+  /* APPLY TO STANDARD DROPDOWN ELEMENTS
+   * =================================== */
+
+  var d = 'a.menu, .dropdown-toggle'
+
+  function clearMenus() {
+    $(d).parent('li').removeClass('open')
+  }
+
+  $(function () {
+    $('html').bind("click", clearMenus)
+    $('body').dropdown( '[data-dropdown] a.menu, [data-dropdown] .dropdown-toggle' )
+  })
+
+}( window.jQuery || window.ender );
